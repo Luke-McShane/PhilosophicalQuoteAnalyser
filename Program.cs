cb3a212fd224ee5f4478210c92e3622975b4e476
Luke McShane <lukejsmcshane@gmail.com>
2024-09-03 18:49:28 +0100

    Implemented most of the CRUD operations for both Quote and Philosopher Repositories

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
 ﻿// Create Philosopher class
 using System.Runtime.CompilerServices;
 
-public class Philosopher(string name, Guid id, string era)
+public class Philosopher(string name, string era)
 {
   public string Name { get; init; } = name;
-  public Guid Id { get; init; } = id;
-  public string Era { get; init; } = era;
-  public
+  public Guid Id { get; set; } = Guid.NewGuid();
+  public string Era { get; set; } = era;
 }
 
 // Create Quote class
-public class Quote(string text, Guid id, Guid philosopherId, IEnumerable<PhilosophicalThemes> themes)
+public class Quote
 {
-  public string Text { get; init; } = text;
-  public Guid Id { get; init; } = id;
-  public Guid PhilosopherId { get; init; } = philosopherId;
-  public IEnumerable<PhilosophicalThemes> Themes { get; init; } = themes;
+  public Quote(string text, Guid philosopher, IEnumerable<PhilosophicalThemes> themes)
+  {
+    Text = text;
+    PhilosopherId = philosopher;
+    Themes = themes;
+
+  }
+  public required string Text { get; init; }
+  public Guid Id { get; set; } = Guid.NewGuid();
+  public Guid PhilosopherId { get; init; }
+  public IEnumerable<PhilosophicalThemes> Themes { get; init; }
 }
 
 public enum PhilosophicalThemes
@@ -34,57 +40,73 @@ public enum PhilosophicalThemes
 }
 
 // Create repository interface
-public interface IRepository<T> where T : class
+public interface IRepository<T, T2> where T : class
 {
   public void Create(T entity);
-  public void Read(T entity);
-  public void Update(T entity);
+  public T Read(T entity);
+  public void Update(T entity, T2 data);
   public void Delete(T entity);
 }
 
 // Implement Philosopher repository
-public class PhilosopherRepository : IRepository<Philosopher>
+public class PhilosopherRepository : IRepository<Philosopher, string>
 {
+  private readonly Dictionary<Guid, Philosopher> _philosophers;
+
+  public PhilosopherRepository()
+  {
+    _philosophers = new Dictionary<Guid, Philosopher>();
+  }
   public void Create(Philosopher entity)
   {
-    throw new NotImplementedException();
+    if (entity.Id == Guid.Empty) entity.Id = Guid.NewGuid();
+    _philosophers[entity.Id] = entity;
   }
 
   public void Delete(Philosopher entity)
   {
-    throw new NotImplementedException();
+    if (_philosophers.ContainsKey(entity.Id)) _philosophers.Remove(entity.Id);
+    else throw new KeyNotFoundException($"{entity.Name} not found.");
   }
 
-  public void Read(Philosopher entity)
+  public Philosopher Read(Philosopher entity)
   {
-    throw new NotImplementedException();
+    if (_philosophers.ContainsKey(entity.Id)) return _philosophers[entity.Id];
+    else throw new KeyNotFoundException($"The key '{entity.Id}' cannot be found.");
   }
 
-  public void Update(Philosopher entity)
+  public void Update(Philosopher entity, string era)
   {
-    throw new NotImplementedException();
+    _philosophers[entity.Id].Era = era;
   }
 }
 
 // Implement Quote repository
-public class QuoteRepository : IRepository<Quote>
+public class QuoteRepository : IRepository<Quote, string>
 {
+  private Dictionary<Guid, Quote> _quotes;
+
+  public QuoteRepository()
+  {
+    _quotes = new Dictionary<Guid, Quote>();
+  }
+
   public void Create(Quote entity)
   {
     throw new NotImplementedException();
   }
 
+  public Quote Read(Quote entity)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Update(Quote entity, string data)
+  {
+    throw new NotImplementedException();
+  }
+
   public void Delete(Quote entity)
-  {
-    throw new NotImplementedException();
-  }
-
-  public void Read(Quote entity)
-  {
-    throw new NotImplementedException();
-  }
-
-  public void Update(Quote entity)
   {
     throw new NotImplementedException();
   }
